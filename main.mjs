92cf4ee3be34cb21a0a7b346a989111344daf7cc
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2022-12-22 00:33:24 +0200

    6.3 uzduotis

---
+++ main.mjs
@@ -0,0 +1,28 @@
+import beep from 'beep'
+import sleep from 'sleep'
+
+//
+// S (...)
+//
+
+// .
+// Taško (ang. dot) trukmė yra 1 laiko vienetas.
+beep(200)
+// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
+sleep(200)
+
+// .
+// Taško (ang. dot) trukmė yra 1 laiko vienetas.
+beep(200)
+// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
+sleep(200)
+
+// .
+// Taško (ang. dot) trukmė yra 1 laiko vienetas.
+beep(200)
+// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
+sleep(200)
+
+// 
+// Papildomas tylos laikotarpis po paskutinio sekos elemento 2 laiko vienetai.
+sleep(400)
