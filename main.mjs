8211cfa5c5a62a78a6213da17740fc22aa79be67
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-02-13 15:47:06 +0200

    9.6 Užduotis

--- main.mjs
+++ main.mjs
@@ -1,27 +1,41 @@
 import beep from 'beep'
 import sleep from 'sleep'
 
+/**
+ * vykdoma funkcija generuoja tasko signala
+ * @returns {undefined}
+ */
+const f_generuoti_signala_taskas = function ()
+{
+    // .
+    // Taško (ang. dot) trukmė yra 1 laiko vienetas.
+    beep(200)
+    // Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
+    sleep(200)
+}
+
+/**
+ * vykdoma funkcija generuoja brūkšnio signala
+ * @returns {undefined}
+ */
+const f_generuoti_signala_bruksnys = function ()
+{
+    // -
+    // Brūkšnio (ang. dash) trukmė yra 3 laiko vienetai.
+    beep(600)
+    // Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
+    sleep(200)
+}
+
 //
 // S (...)
 //
 
-// .
-// Taško (ang. dot) trukmė yra 1 laiko vienetas.
-beep(200)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_taskas()
 
-// .
-// Taško (ang. dot) trukmė yra 1 laiko vienetas.
-beep(200)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_taskas()
 
-// .
-// Taško (ang. dot) trukmė yra 1 laiko vienetas.
-beep(200)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_taskas()
 
 // 
 // Papildomas tylos laikotarpis po paskutinio sekos elemento 2 laiko vienetai.
@@ -31,23 +45,11 @@ sleep(400)
 // O (---)
 //
 
-// -
-// Brūkšnio (ang. dash) trukmė yra 3 laiko vienetai.
-beep(600)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_bruksnys()
 
-// -
-// Brūkšnio (ang. dash) trukmė yra 3 laiko vienetai.
-beep(600)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_bruksnys()
 
-// -
-// Brūkšnio (ang. dash) trukmė yra 3 laiko vienetai.
-beep(600)
-// Tylos laikotarpis po kiekvieno taško ar brūkšnio signalo yra 1 laiko vienetas.
-sleep(200)
+f_generuoti_signala_bruksnys()
 
 // 
 // Papildomas tylos laikotarpis po paskutinio sekos elemento 2 laiko vienetai.
